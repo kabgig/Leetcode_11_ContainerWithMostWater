b5e4cb0eb18fa3d08d484b479760b1e5042059dd
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-13 17:09:11 +0400

    almost done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] lines = {1,2,1,1,2};
+        Solution solution = new Solution();
+        int res = solution.maxArea(lines);
+        System.out.println(res);
     }
 }
+
+/*
+
+Constraints:
+
+        n == height.length
+        2 <= n <= 105
+        0 <= height[i] <= 104
+
+        */
---
+++ src/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSquare;
+    public int maxArea(int[] height) {
+        for (int i = 0; i < height.length; i++) {
+            for (int j = 0; j <height.length; j++) {
+                if(i == j) continue;
+                if (height[i] < height[j])
+                    square(height[i],Math.abs(i-j) );
+                else square(height[j],Math.abs(i-j) );
+            }
+        }
+        return maxSquare;
+    }
+
+    public void square(int x, int y) {
+        int square = x * y;
+        if (maxSquare < square) maxSquare = square;
+    }
+}
